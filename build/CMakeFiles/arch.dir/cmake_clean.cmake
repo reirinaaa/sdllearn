de5a0f404b152aefefac561d547da6ee9e73bb4f
reirinaaa <reirinaa@proton.me>
2025-08-24 18:12:53 +0700

    first commit

---
+++ build/CMakeFiles/arch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arch.dir/arch.c.obj"
+  "CMakeFiles/arch.dir/arch.c.obj.d"
+  "arch.exe"
+  "arch.exe.manifest"
+  "arch.pdb"
+  "libarch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
